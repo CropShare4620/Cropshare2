85c39dc22db3a4c4a7bafd69161d95d25471749d
Michael Lieb <mal.lieb121@gmail.com>
2020-09-14 18:48:44 -0400

    Optimized smooth scrolling, js->css

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).render('404');
 });
 
 // error handler
